e9a63c5d656577184e3ce76619286527ab56c044
Ninja Hybrid Fornite <43005960+ShinobiDracoBalder@users.noreply.github.com>
2021-08-23 19:07:14 -0500

    Changed Account

---
+++ Vehicles.API/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vehicles.API.Data;
+
+namespace Vehicles.API.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly DataContext _dataContext;
+
+        public AccountController(DataContext context)
+        {
+            _dataContext = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Vehicles.API/Data/SeedDb.cs
+++ Vehicles.API/Data/SeedDb.cs
@@ -1,6 +1,19 @@
-﻿namespace Vehicles.API.Data
+﻿using System.Threading.Tasks;
+
+namespace Vehicles.API.Data
 {
     public class SeedDb
     {
+        private readonly DataContext _dataContext;
+
+        public SeedDb(DataContext context)
+        {
+            _dataContext = context;
+        }
+        public async Task SeedAsync()
+        {
+            await _dataContext.Database.EnsureCreatedAsync();
+            
+        }
     }
 }
